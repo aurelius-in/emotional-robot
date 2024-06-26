e9e172ee4c977f6c84b9268e840902a809bd55c3
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2024-06-25 19:38:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const video = document.getElementById('video');
 const canvas = document.getElementById('overlay');
+const context = canvas.getContext('2d');
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -9,27 +10,27 @@ Promise.all([
 ]).then(startVideo);
 
 function startVideo() {
-    navigator.mediaDevices.getUserMedia({
-        video: {}
-    }).then(stream => {
-        video.srcObject = stream;
-    }).catch(err => console.error(err));
+    navigator.mediaDevices.getUserMedia({ video: {} })
+        .then(stream => {
+            video.srcObject = stream;
+        })
+        .catch(err => console.error("Error accessing webcam: ", err));
 }
 
 video.addEventListener('play', () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
     const displaySize = { width: video.videoWidth, height: video.videoHeight };
     faceapi.matchDimensions(canvas, displaySize);
+
     setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceExpressions();
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        context.clearRect(0, 0, canvas.width, canvas.height);
         faceapi.draw.drawDetections(canvas, resizedDetections);
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
         faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
     }, 100);
-});
-
-video.addEventListener('loadedmetadata', () => {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
 });
