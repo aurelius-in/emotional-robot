88f405c1466adea17911078a6490fc70e0f8b7eb
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2024-06-25 21:30:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ function logMessage(message) {
 
 async function startCamera() {
     try {
+        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+            throw new Error('Browser does not support media devices API.');
+        }
+        
+        const permissions = await navigator.permissions.query({ name: 'camera' });
+        logMessage(`Camera permission state is ${permissions.state}`);
+
         const stream = await navigator.mediaDevices.getUserMedia({ video: true });
         video.srcObject = stream;
         logMessage('Camera started successfully.');
